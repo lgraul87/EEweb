c19a4ab6dfee784072612c357ea8c83d9befb1f1
Raul Lora Gomez <lgraul87@gmail.com>
2020-10-18 16:01:58 +0200

    1 paso

---
+++ src/main/java/servletpackage/Servlet001.java
@@ -0,0 +1,41 @@
+package servletpackage;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet001
+ */
+public class Servlet001 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet001() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		System.out.println("Prueba");
+		System.out.println(request.getParameter("Seleccion"));
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>App Calcula nominas</title>
+</head>
+<body>
+<h1>Bienvenido a la App Calcula-nominas</h1>
+<h2>Elija opcion: </h2>
+<form action="Servlet001" method="post">
+<table>
+<tr>
+<td>Entrar a la app</td>
+<td> <input type="radio" name="Seleccion" value="Entrar a la app"/></td>
+</tr>
+<tr>
+<td>Entrar a la demo</td>
+<td> <input type="radio" name="Seleccion" value="Entrar a la demo"/></td>
+</tr>
+<tr>
+<td>Salir</td>
+<td><input type="radio" name="Seleccion" value="Salir"/></td>
+</tr>
+<tr>
+<td><input type="submit" value="Enviar"></td>
+</tr>
+</table>
+  </form>
+</body>
+</html>
